3e5482d04b23b612fcdc25be71e5712d1b6172f2
briannagautama <briannagautama@gmail.com>
2024-03-19 02:42:05 -0700

    Update Pharmacy.js

--- src/components/Pharmacy.js
+++ src/components/Pharmacy.js
@@ -93,7 +93,7 @@ const Pharmacy = () => {
 
     if (web3 && accounts && contract) {
       retrieveInventory();
-      console.log(inventory[0].name);
+      console.log("Drug name:", inventory[0].name);
       // calculatePrice();
     }
   }, [web3, accounts, contract]); // Add dependencies to useEffect
