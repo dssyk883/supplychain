8e8a676cda99d70408819add45d4be6a85ff4e1d
dssyk883 <soyeonkwon.0@gmail.com>
2024-03-20 14:22:27 -0700

    hh2

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-<<<<<<< HEAD
-require("@nomicfoundation/hardhat-ethers");     
-=======
 require("@nomicfoundation/hardhat-ethers");
->>>>>>> 7d99f64 (hardhat)
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 paths: {
     artifacts: "./src/artifacts",
   },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 2d074f6 (hardhat)
 allowUnlimitedContractSize: true,
 networks: {
                 localhost: {
@@ -38,71 +29,6 @@ namedAccounts: {
 		default: 0,
 		amount: "10000000000000000000000"
 		},
-<<<<<<< HEAD
-},
-=======
-=======
-=======
->>>>>>> 7d99f64 (hardhat)
-networks: {
-                localhost: {
-                        url: "http://ec2-54-219-16-72.us-west-1.compute.amazonaws.com:8545",
-                        chainID: 1337
-                },
-                hardhat: {
-                        chainID: 1337
-                }
-},
-<<<<<<< HEAD
-namedAccounts: { 
-=======
-namedAccounts: {
->>>>>>> 7d99f64 (hardhat)
-    deployer: {
-        default: 0,
-        amount: "10000000000000000000000"
         },
-<<<<<<< HEAD
->>>>>>> 002b6ca (hardhat)
-=======
->>>>>>> 7d99f64 (hardhat)
-},
 
->>>>>>> 2d074f6 (hardhat)
-solidity: {
-        version: "0.8.24",
-        settings: {
-                optimizer: {
-                        enabled: true,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 2d074f6 (hardhat)
-                        runs: 100000,
-			 details: { yul: false },
-        }
-},
-},
-
-<<<<<<< HEAD
 };
-=======
-};
-
-=======
-                        runs: 1000,
-        }
-=======
-                        runs: 1000,
-			}
->>>>>>> 7d99f64 (hardhat)
-},
-},
-allowUnlimitedContractSize: true,
-};
-<<<<<<< HEAD
->>>>>>> 002b6ca (hardhat)
-=======
->>>>>>> 7d99f64 (hardhat)
->>>>>>> 2d074f6 (hardhat)
