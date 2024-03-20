79e95484a93164308f65cf241fe1ee1542dcee3f
dssyk883 <soyeonkwon.0@gmail.com>
2024-03-19 22:09:12 -0700

    addinitialdrugs in WD

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -95,7 +95,7 @@ contract SupplyChain is Pharmacy, Manufacturer, Wholesale, Insurer {
         addDrugInPH(0, 10, address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0x90F79bf6EB2c4f870365E785982E1f101E93b906));
         addDrugInPH(1, 5, address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0x90F79bf6EB2c4f870365E785982E1f101E93b906));
         addDrugInPH(1, 5, address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), address(0x90F79bf6EB2c4f870365E785982E1f101E93b906));
-        
+        addInitialDrugsWD();
         addInitialDrugsMA();
         addInitialDiscounts();
     }
