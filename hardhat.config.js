d7b3457d1ddc43647e859c143b93380ebfa19474
Ubuntu <ubuntu@ip-172-31-1-199.us-west-1.compute.internal>
2024-03-20 21:13:36 +0000

    changed ip

---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");     
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+paths: {
+    artifacts: "./src/artifacts",
+  },
+allowUnlimitedContractSize: true,
+networks: {
+                localhost: {
+                        url: "http://ec2-54-219-16-72.us-west-1.compute.amazonaws.com:8545",
+                        chainID: 1337,
+			allowUnlimitedContractSize: true,
+			gas: 12000000,
+			blockGasLimit: 0x1fffffffffffff,
+		        ignoreUnknownTxType: true,
+                },
+                hardhat: {
+                        chainID: 1337,
+			allowUnlimitedContractSize: true,
+			gas: 12000000,
+		        blockGasLimit: 0x1fffffffffffff,
+	                ignoreUnknownTxType: true,
+                }
+},
+namedAccounts: { 
+	deployer: {
+		default: 0,
+		amount: "10000000000000000000000"
+		},
+},
+
+solidity: {
+        version: "0.8.24",
+        settings: {
+                optimizer: {
+                        enabled: true,
+                        runs: 100000,
+			 details: { yul: false },
+        }
+},
+},
+
+};
+
